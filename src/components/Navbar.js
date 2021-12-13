ff4843d3bbad060a3fc1b89460bd7509f03f3825
sja_thedude <sja.affu765@gmail.com>
2021-12-13 15:36:51 +0300

    Updated file with Nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,15 @@ const Navbar = () => (
       <img className="img-logo" src={logo} alt="planet logo" />
       <span className="text-logo">Space Travelers&apos; Hub</span>
     </div>
+    <div className="nav-links">
+      <ul className="link-list">
+        <li className="link-item">
+          <NavLink to="/my-profile">
+            My Profile
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   </div>
 );
 
