857dfa977e737fe17b4ca5efb5b2de13fd30db75
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2021-12-13 20:48:49 -0800

    Update dragon.js

--- src/redux/reducers/dragons.js
+++ src/redux/reducers/dragons.js
@@ -1,14 +1,22 @@
+/* eslint-disable no-case-declarations */
 import { FETCH_ALL_DRAGONS } from '../actions/actionTypes';
 
-const reducer = (state = [], action) => {
+const dragons = (state = [], action) => {
   switch (action.type) {
     case FETCH_ALL_DRAGONS:
-      // Write code here
-      return state; // TODO: return 'action.payload'
+      const filteredData = action.payload.map((dragon) => {
+        const {
+          id, dragon_name: name, dragon_type: type, flickr_images: image, description,
+        } = dragon;
+        return {
+          id, name, type, image, description,
+        };
+      });
+      return [...state, ...filteredData];
 
     default:
       return state;
   }
 };
 
-export default reducer;
+export default dragons;
