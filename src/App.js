50cb6c3fdcb24714a33b3fafca78bf1b75787b05
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2021-12-13 20:44:22 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Line from './components/Line';
 import Rockets from './components/Rockets';
+import Dragons from './components/dragons/Dragons';
 import MyProfile from './components/MyProfile';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Line />
       <Routes>
         <Route path="/rockets" element={<Rockets />} />
+        <Route path="/dragons" element={<Dragons />} />
         <Route path="/my-profile" element={<MyProfile />} />
         <Route path="/" element={<Rockets />} />
       </Routes>
